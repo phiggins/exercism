7ee6b572361633fb9df4746fdcc0d0ef835f49cc
Pete Higgins <pete@peterhiggins.org>
2016-08-22 01:02:56 -0700

    robot-name: Remove use of State monad with ridiculous one-liner.

--- haskell/robot-name/Robot.hs
+++ haskell/robot-name/Robot.hs
@@ -1,22 +1,15 @@
 module Robot where
-  import Control.Monad.State
   import System.Random
 
   mkRobot :: IO StdGen
   mkRobot = newStdGen
 
   robotName :: StdGen -> IO String
-  robotName r = return $ fst $ runState genRobotName r
+  robotName r = return $ genRobotName r
 
   resetName = undefined
 
-  genRobotName :: State StdGen String
-  genRobotName = do
-    letter1 <- randomLetter
-    letter2 <- randomLetter
-    digit1 <- randomDigit
-    digit2 <- randomDigit
-    digit3 <- randomDigit
-    return [letter1, letter2, digit1, digit2, digit3]
-    where randomLetter = state $ randomR ('A', 'Z')
-          randomDigit = state $ randomR ('0', '9')
+  genRobotName :: StdGen -> String
+  genRobotName r = fst $ foldl (\(s,g') f -> let (l,g'') = f g' in (l:s,g'')) ("", r) [randomLetter, randomLetter, randomDigit, randomDigit, randomDigit]
+    where randomLetter = randomR ('A', 'Z')
+          randomDigit = randomR ('0', '9')
