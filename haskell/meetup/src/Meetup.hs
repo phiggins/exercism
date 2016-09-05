582b7dbb039ab2540b52324feb9608258c80b698
Pete Higgins <pete@peterhiggins.org>
2016-09-05 16:14:11 -0700

    meetup: cut down on logic by handling all schedules the same way.

--- haskell/meetup/src/Meetup.hs
+++ haskell/meetup/src/Meetup.hs
@@ -3,25 +3,22 @@ module Meetup (Weekday(..), Schedule(..), meetupDay) where
 import Data.Time.Calendar (Day, fromGregorian, toGregorian)
 import Data.Time.Calendar.WeekDate (toWeekDate)
 
--- The task is to create the data types `Weekday` and
--- `Schedule`, and implement the function `meetupDay`.
-
 data Schedule = Teenth | First | Second | Third | Fourth | Last
 data Weekday = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 
 meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
-meetupDay Teenth weekday year month = teenth $ daysOfMonth year month weekday
-meetupDay First weekday year month = head $ daysOfMonth year month weekday
-meetupDay Second weekday year month = last . take 2 $ daysOfMonth year month weekday
-meetupDay Third weekday year month = last . take 3 $ daysOfMonth year month weekday
-meetupDay Fourth weekday year month = last . take 4 $ daysOfMonth year month weekday
-meetupDay Last weekday year month = last $ daysOfMonth year month weekday
+meetupDay schedule weekday year month =
+  let days = map (fromGregorian year month) (dayRange schedule)
+  in head $ filter (isDay weekday) days
 
-teenth :: [Day] -> Day
-teenth xs = head $ filter (\x -> let (_,_,n) = toGregorian x in n > 12 && n < 20) xs
-
-daysOfMonth :: Integer -> Int -> Weekday -> [Day]
-daysOfMonth year month weekday = filter (isDay weekday) (allDaysOfMonth year month)
+dayRange :: Schedule -> [Int]
+dayRange schedule = case schedule of
+  Teenth  -> [13..19]
+  First   -> [1..7]
+  Second  -> [8..14]
+  Third   -> [15..21]
+  Fourth  -> [22..28]
+  Last    -> reverse [21..31]
 
 isDay :: Weekday -> Day -> Bool
 isDay Monday day      = dayOfWeek day == 1
@@ -36,6 +33,3 @@ dayOfWeek :: Day -> Int
 dayOfWeek day =
   let (_,_,n) = toWeekDate day
   in n
-
-allDaysOfMonth :: Integer -> Int -> [Day]
-allDaysOfMonth year month = map (fromGregorian year month) [1..31]
